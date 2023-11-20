30d18afa5c653415f8edf70c881d50f55b877b32
HeelHook88 <Heel@Hook88>
2023-11-20 12:41:24 +0300

    add .env

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     packages=find_packages(),
     version='0.1.0',
     description='A short description of the project.',
-    author='Andrey_Kuznetsov',
+    author='HeelHook88',
     license='MIT',
 )
